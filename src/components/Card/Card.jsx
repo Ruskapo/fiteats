ea6b037947a50170e920a903bfa484f3f43d528e
Alexandr Lebedev <cockato0.lae@gmail.com>
2025-12-23 22:48:55 +0500

    feat: render recipes list with nutrition info

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-
 import styles from "./Card.module.scss";
 
-const Card = ({ title }) => {
+const Card = ({ title, calories, protein, fat, carbs, time }) => {
   return (
     <div className={styles.card}>
       <h3 className={styles.title}>{title}</h3>
+      <div className={styles.meta}>
+        <span>{calories} ккал </span>
+        <span> {time} мин </span>
+      </div>
+      <div className={styles.macros}>
+        <span className={styles.badge}>Б: {protein} г</span>
+        <span className={styles.badge}>Ж: {fat} г</span>
+        <span className={styles.badge}>У: {carbs} г</span>
+      </div>
     </div>
   );
 };
--- src/pages/Recipes/Recipes.jsx
+++ src/pages/Recipes/Recipes.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
+import recipes from "../../data/recipes";
+import Card from "../../components/Card/Card";
+import styles from "./Recipes.module.scss";
 
 const Recipes = () => {
   return (
-    <div>Recipes</div>
-  )
-}
-
+    <div className={styles.container}>
+      {recipes.map((recipe) => (
+        <Card
+          key={recipe.id}
+          title={recipe.title}
+          calories={recipe.calories}
+          protein={recipe.protein}
+          fat={recipe.fat}
+          time={recipe.time}
+          carbs={recipe.carbs}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Recipes;
