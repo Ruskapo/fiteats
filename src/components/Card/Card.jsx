5eb4bf98fc9263b01aacac34b0287ca7bb683a32
Alexandr Lebedev <cockato0.lae@gmail.com>
2026-01-08 23:18:23 +0500

    feat: add favorites toggle button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,38 @@
-import styles from "./Card.module.scss";
 import { Link } from "react-router-dom";
+import styles from "./Card.module.scss";
 
-const Card = ({ title, calories, protein, fat, carbs, time, id }) => {
+const Card = ({
+  title,
+  calories,
+  protein,
+  fat,
+  carbs,
+  time,
+  id,
+  isFavorite,
+  onToggleFavorite,
+}) => {
   return (
     <Link to={`/recipe/${id}`}>
-    <div className={styles.card}>
-      <h3 className={styles.title}>{title}</h3>
-      <div className={styles.meta}>
-        <span>{calories} ккал </span>
-        <span> {time} мин </span>
+      <div className={styles.card}>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            onToggleFavorite(id);
+          }}
+          className={styles.favBtn}> ★ </button>
+        {isFavorite ? "⭐" : "☆"}
+        <h3 className={styles.title}>{title}</h3>
+        <div className={styles.meta}>
+          <span>{calories} ккал </span>
+          <span> {time} мин </span>
+        </div>
+        <div className={styles.macros}>
+          <span className={styles.badge}>Б: {protein} г</span>
+          <span className={styles.badge}>Ж: {fat} г</span>
+          <span className={styles.badge}>У: {carbs} г</span>
+        </div>
       </div>
-      <div className={styles.macros}>
-        <span className={styles.badge}>Б: {protein} г</span>
-        <span className={styles.badge}>Ж: {fat} г</span>
-        <span className={styles.badge}>У: {carbs} г</span>
-      </div>
-    </div>
     </Link>
   );
 };
