862672dda3701a8733593168440767613151b5a9
Alexandr Lebedev <cockato0.lae@gmail.com>
2026-01-08 22:27:42 +0500

    feat: add favorites state add toggel handler

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AppContext = () => {
+  const [favorites, setFavorites] = useState([]);
+  const toggleFavorite = (id) => {
+    if (favorites.includes(id)) {
+      setFavorites(favorites.filter((itemId) => itemId !== id));
+    
+    } else {
+      setFavorites([...favorites, id]);
+    }
+  }
   return (
     <div>AppContext</div>
   )
