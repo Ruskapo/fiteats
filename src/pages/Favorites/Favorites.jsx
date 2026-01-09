ed3155cc6f94e8bafa70a4f69fca1047c64b2445
Alexandr Lebedev <cockato0.lae@gmail.com>
2026-01-09 20:34:49 +0500

    favorites page with context and ui

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import { useContext } from "react";
+import Card from "../../components/Card/Card";
+import { AppContext } from "../../context/AppContext";
+import recipes from "../../data/recipes";
+import styles from "./Favorites.module.scss";
 
 const Favorites = () => {
-  return (
-    <div>Favorites</div>
-  )
-}
+  const { favorites, toggleFavorite } = useContext(AppContext);
 
+  // Это ключевая логика всего избранного
+  // recipes-весь массив,filter-пробегается по каждому рецепту,favorites.includes(recipe.id)-оставляет только те чей id есть в избранном
+  const favoriteRecipes = recipes.filter((recipe) =>
+    favorites.includes(recipe.id)
+  );
+
+  return (
+    <>
+      <div className={styles.page}>
+        <div className={styles.title}>Избранное</div>
+        <span className={styles.count}>{favoriteRecipes.length}</span>
+      </div>
+
+      {favoriteRecipes.length === 0 && (
+        <div className={styles.empty}>
+          <h3 className={styles.emtyTitle}>Избранное пусто</h3>
+          <p className={styles.emtyText}>Добавь рецепты, нажав ⭐ </p>
+        </div>
+      )}
+
+      {favoriteRecipes.length > 0 && (
+        <div className={styles.grid}>
+          {favoriteRecipes.map((recipe) => (
+            <Card
+              key={recipe.id}
+              id={recipe.id}
+              title={recipe.title}
+              calories={recipe.calories}
+              protein={recipe.protein}
+              fat={recipe.fat}
+              time={recipe.time}
+              carbs={recipe.carbs}
+              isFavorite={favorites.includes(recipe.id)}
+              onToggleFavorite={toggleFavorite}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default Favorites;
