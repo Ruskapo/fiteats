3147226e0a5d98972af04d741fa2d856581e8d38
Alexandr Lebedev <cockato0.lae@gmail.com>
2026-03-13 22:01:14 +0500

    feat(ui): animated app header + active navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,56 @@
+import { motion } from "motion/react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.top}>
+      <motion.div
+        className={styles.top}
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
         <Link to="/" className={styles.logo}>
           FitEats
         </Link>
-      </div>
+      </motion.div>
 
-      <div className={styles.navbar}>
-        <NavLink className={styles.link} to="/app">
+      <motion.div
+        className={styles.navbar}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+      >
+        <NavLink
+          to="/app"
+          end
+          className={
+            ({ isActive }) =>
+              isActive ? `${styles.link} ${styles.active}` : styles.link // Подсветка выбранной страницы
+          }
+        >
           Главная
         </NavLink>
 
-        <NavLink className={styles.link} to="/app/recipes">
+        <NavLink
+          to="/app/recipes"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
           Рецепты
         </NavLink>
 
-        <NavLink className={styles.link} to="/app/favorites">
+        <NavLink
+          to="/app/favorites"
+          className={({ isActive }) =>
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+        >
           Избранное
         </NavLink>
-      </div>
+      </motion.div>
     </header>
   );
 };
